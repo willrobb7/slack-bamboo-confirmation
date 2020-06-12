53513386f6a5125f7ec41f4016491d05b48b6347
willrobb7 <will.robinson@infinityworks.com>
2020-06-12 15:21:12 +0100

    Removed un-needed lines from deploy.sh & requirements.txt

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 CWD=$(pwd)
-packages=$(pip freeze | sed -E "s/=.*//")
 
 echo "Removing old zip if present"
 rm function.zip
@@ -11,7 +10,5 @@ cd $CWD
 
 zip -r "${CWD}/function.zip" *.py message_format.json
 echo "Updating function in AWS"
-
-echo $packages
 
 aws lambda update-function-code --profile sts --region eu-west-2 --function-name slack-bamboo-confirmation-dev --zip-file fileb://function.zip
